a620f8eb8f7d2322ac5e61e5af324072c0c74605
Dewa Anugrah Aljas Putra <dewaanugrahaljasputra.2003@gmail.com>
2025-03-08 12:56:22 +0800

    -

--- quiz/ujian/soal1_ujian.js
+++ quiz/ujian/soal1_ujian.js
@@ -5,16 +5,51 @@ maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama seka
 */
 function targetTerdekat(arr) {
   // you can only write your code here!
-  let counter = 0;
-  
+
+  let posisiSekarang = 0; // Inisialisasi posisi saat ini
+  let foundX = false; // Menandakan apakah 'x' sudah ditemukan
+
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 'x') {
-        counter += 0;
+    if (arr[i] === 'o') {
+      posisiSekarang = 0; // Reset posisi saat ini ke 0 jika menemukan 'o'
+    } else if (arr[i] === 'x') {
+      foundX = true; // Tandai bahwa 'x' sudah ditemukan
+      posisiSekarang++;
+      break; // Hentikan loop setelah menemukan 'x'
+    } else {
+      posisiSekarang++; // Tambah posisi saat ini jika bukan 'o' atau 'x'
     }
   }
 
-  return counter;
+  if (!foundX) {
+    posisiSekarang = 0;
+  }
+
+  return posisiSekarang;
 }
+
+// let arr = [' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'];
+
+// let posisiO = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 'o') {
+//     posisiO = i;
+//   } else if (arr[i] === 'x' && posisiO != -1) {
+//     let posisi = Math.abs(posisiO - i);
+//     if (posisi < jarak) {
+//       jarak = posisi;
+//     }
+//   }
+// }
+
+// console.log(posisiO);
+// console.log(jarak);
+
+// let arr = [' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'];
+
+// console.log("Posisi Sekarang:", posisiSekarang); // Ini akan mengembalikan posisi saat ini
+// console.log("Apakah 'x' ditemukan?", foundX); // Ini akan mengembalikan true jika 'x' ditemukan
 
 // TEST CASES
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
